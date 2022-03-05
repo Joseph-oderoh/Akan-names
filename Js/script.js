70e199697fde3e1023737f741b6c0368f1030f41
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-03-05 15:07:53 +0300

    refactor:change html js  file

--- Js/script.js
+++ Js/script.js
@@ -7,14 +7,15 @@ let maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 let femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
 const form = document.getElementById("form");
-const year = document.getElementById("year");
+const year  = document.getElementById("year");
 const month = document.getElementById("month");
 const male = document.getElementById("male");
 const female = document.getElementById("female");
 
-
-
-
- 
-    
-  
+function handleForm(event) {
+     event.preventDefault(); 
+    } 
+form.addEventListener('submit', handleForm);
+ function myFunction(){
+const form = document.getElementById("form");
+ }
