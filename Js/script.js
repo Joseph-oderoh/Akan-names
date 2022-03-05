e22e23aafd7592938661a352547ee8f71e5e6e99
Joseph_Oderoh <joseph.oderoh@student.moringaschool.com>
2022-03-05 08:25:56 +0300

    style:body-optimized js styling

---
+++ Js/script.js
@@ -0,0 +1,18 @@
+let heading = "Akan";
+document.title = "Akan";
+
+
+let dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+let maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+let femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+let form = document.getElementById("form");
+ let year =   document.getElementById("year");
+form.addEventListener('submit', (e)=>{
+     e.preventDefault()
+  
+     if (year.value == ""){
+         alert("fill in the form");
+     }
+     
+});
